03a6279fd4ba9a38203bd24179293059492e9112
Stanislau Stasheuski <stanislau.stasheuski@gmail.com>
2022-11-14 15:38:38 +0200

    doom: draft an org-roam setup

--- .doom.d/packages.el
+++ .doom.d/packages.el
@@ -15,6 +15,10 @@
 ;(package! another-package
 ;  :recipe (:host github :repo "username/repo"))
 
+;; (package! org-roam
+;;   :recipe (:host github :repo "org-roam/org-roam"
+;;            :files (:defaults "extensions/*")))
+
 ;; If the package you are trying to install does not contain a PACKAGENAME.el
 ;; file, or is located in a subdirectory of the repo, you'll need to specify
 ;; `:files' in the `:recipe':
