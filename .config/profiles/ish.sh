0b5b1b5f6eea0a2c683dc7c769c21d3f47668b0d
Stanislau Stasheuski <stanislau.stasheuski@gmail.com>
2023-11-04 22:40:57 +0000

    `sh`: create configuration for `ish`

---
+++ .config/profiles/ish.sh
@@ -0,0 +1,12 @@
+# vim:fileencoding=utf-8:foldmethod=marker
+# t21202-lr017 machine (Aalto Linux) specific settings
+
+#: Functions {{{
+#: }}}
+
+#: Exports {{{
+#: }}}
+
+#: Aliases {{{
+alias lf='lf -single'
+#: }}}
