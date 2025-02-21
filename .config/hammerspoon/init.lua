1f4040ee89bcfa3ed75ccbd2e7a73409879c8edd
Stanislau Stasheuski <stanislau.stasheuski@gmail.com>
2025-02-21 08:50:39 +0200

    🔨: fix toggle dark mode in kitty

--- .config/hammerspoon/init.lua
+++ .config/hammerspoon/init.lua
@@ -1,33 +1,57 @@
 --[[
 defaults write org.hammerspoon.Hammerspoon MJConfigFile "~/.config/hammerspoon/init.lua"
 for spoon in <spoons>; do
-  curl -LO $spoon --output-dir S~/.config/hammerspoon/Spoons/
+  curl -LO $spoon --output-dir ~/.config/hammerspoon/Spoons/
 done
 ]]
 
 hs = hs -- prevents warnings
+primaryShortcut = {"ctrl", "alt", "shift"}
 
-hs.hotkey.bind({"ctrl", "alt", "shift"}, "T", function()
-  local _, isDarkMode = hs.osascript.applescript([[
+hs.hotkey.bind(primaryShortcut, "T", function()
+  hs.application.launchOrFocus("Kitty")
+end)
+
+hs.hotkey.bind(primaryShortcut, "D", function()
+  local _, isLightMode = hs.osascript.applescript([[
     tell application "System Events"
       tell appearance preferences
-        set darkModeState to dark mode
+        set lightModeState to dark mode
         set dark mode to not dark mode
       end tell
     end tell
-    return darkModeState
+    return lightModeState
   ]])
 
-  local theme = isDarkMode and "Dracula" or "Goph Mar"
-  hs.task.new(
-    "/run/current-system/sw/bin/kitty",
-    nil,
-    {"+kitten", "themes", "--reload-in=all",theme}
-  ):start()
-  hs.task.new("/bin/sh", nil, {"-c", "pkill -30 -a kitty"}):start()
+  -- kitty
+  hs.task.new("/run/current-system/sw/bin/kitty", nil, {
+    "+kitten",
+    "themes",
+    "--reload-in=all",
+    isLightMode and "Goph Mar" or "Dracula",
+  }):start()
+  hs.timer.doAfter(0.25, function()
+    hs.task.new("/bin/sh", nil, {"-c", "pkill -30 -a kitty"}):start()
+  end)
+
+  -- bat
+  hs.task.new("/bin/sh", nil, {"-c", (
+      isLightMode
+      and "sed -i'.prev' 's/theme=\"Dracula\"/theme=\"GitHub\"/g' "
+      or "sed -i'.prev' 's/theme=\"GitHub\"/theme=\"Dracula\"/g' "
+    ) .. os.getenv("HOME") .. "/.config/bat/config",
+  }):start()
+
+  -- helix
+  hs.task.new("/bin/sh", nil, {"-c", (
+      isLightMode
+      and "sed -i'.prev' 's/theme = \"term16_dark\"/theme = \"term16_light\"/g' "
+      or "sed -i'.prev' 's/theme = \"term16_light\"/theme = \"term16_dark\"/g' "
+    ) .. os.getenv("HOME") .. "/.config/helix/config.toml",
+  }):start()
 end)
 
-hs.hotkey.bind({"ctrl", "alt", "shift"}, "R", function()
+hs.hotkey.bind(primaryShortcut, "R", function()
   hs.reload()
 end)
--- hs.alert.show("🔨")
+hs.alert.show("🔨")
