f2dcc07203077ff78de9aa9c929a2bd3445abd4d
Stanislau Stasheuski <stanislau.stasheuski@gmail.com>
2025-07-31 16:47:06 +0300

    `OpenFOAM`: add .env-file for maari

--- zsh/env-linux.sh
+++ zsh/env-linux.sh
@@ -1,5 +1,7 @@
 # vim:fileencoding=utf-8:foldmethod=marker
-#$ ln -sfn env-maari.sh $HOME/.config/zsh/.zprofile
+#$ ln -sfn env-linux.sh $HOME/.config/zsh/.zprofile
+
+export SPACK_ROOT="${HOME}/.local/opt/spack"
 
 export PIPX_HOME="${HOME}/.local/share/pipx"
 export PYENV_ROOT="${HOME}/.local/share/pyenv"
