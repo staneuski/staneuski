709f4288cfb1d7e23a117059f84a06a0c089bce1
Stanislau Stasheuski <stanislau.stasheuski@gmail.com>
2025-08-17 20:32:00 +0300

    `shell`: guess LUMI environment by user name

--- zsh/env-lumi.sh
+++ zsh/env-lumi.sh
@@ -1,7 +1,9 @@
 # vim:fileencoding=utf-8:foldmethod=marker
 #$ ln -sfn env-lumi.sh $HOME/.config/zsh/.zprofile
 
-export SBATCH_ACCOUNT='project_462000885'
+[ "${USER}" == "stashes1" ] &&
+    export SBATCH_ACCOUNT='project_462000929' ||
+    export SBATCH_ACCOUNT='project_462000885'
 export SBATCH_ALLOC="${SBATCH_ACCOUNT}"
 
 export RIP_GRAVEYARD="/scratch/${SBATCH_ACCOUNT}/${USER}/.graveyard"
