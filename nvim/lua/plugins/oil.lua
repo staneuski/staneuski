56cdd3b03403df3e5e81b61c64ded9615d62baed
Stanislau Stasheuski <stanislau.stasheuski@gmail.com>
2026-01-02 00:02:38 +0200

    `nvim`: always float `oil.nvim` with preview window on

--- nvim/lua/plugins/oil.lua
+++ nvim/lua/plugins/oil.lua
@@ -1,3 +1,6 @@
+-- https://github.com/stevearc/oil.nvim?tab=readme-ov-file#options
+local preview = { preview = { vertical = true } }
+
 return {
   "stevearc/oil.nvim",
   priority = 1000,
@@ -7,30 +10,46 @@ return {
   ---@type oil.SetupOpts
   opts = {
     default_file_explorer = false,
-    view_options = {
-        show_hidden = true,
-        is_always_hidden = function(name, bufnr)
-            local m = name:match("^..$")
-            return m ~= nil
-        end,
+    keymaps = {
+      ["q"] = { "actions.close", mode = "n" },
     },
+    view_options = {
+      show_hidden = true,
+      is_always_hidden = function(name, bufnr)
+        local m = name:match("^..$")
+        return m ~= nil
+      end,
+    },
+    float = {
+      max_width = 0.8,
+      max_height = 0.8,
+      border = "rounded",
+    },
+    confirmation = { border = "rounded" },
+    progress = { border = "rounded" },
   },
   keys = {
     {
       "-",
-      function() require("oil").open() end,
+      function()
+        require("oil").open_float(nil, preview)
+      end,
       mode = { "n" },
-      desc = "Navigate to the parent path",
+      desc = "Open oil (Directory of the Current File)",
     },
     {
       "<leader>fo",
-      function() require("oil").open() end,
-      desc = "Open oil (Directory of Current File)",
+      function()
+        require("oil").open_float(nil, preview)
+      end,
+      desc = "Open oil (Directory of the Current File)",
     },
     {
       "<leader>fO",
-      function() require("oil").open(vim.uv.cwd()) end,
-      desc = "Open oil (cwd)",
+      function()
+        require("oil").open_float(LazyVim.root(), preview)
+      end,
+      desc = "Open oil (root dir)",
     },
   },
 }
