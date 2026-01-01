6154402b31283cf3ad11430ead3b66c301c0f828
Stanislau Stasheuski <stanislau.stasheuski@gmail.com>
2026-01-01 18:34:03 +0200

    `nvim`: remove LazyVim header from dashboard

--- nvim/lua/plugins/default.lua
+++ nvim/lua/plugins/default.lua
@@ -5,7 +5,14 @@ return {
     enabled = false,
   },
   {
+    "folke/snacks.nvim",
+    opts = {
+      -- https://github.com/folke/snacks.nvim/blob/main/docs/dashboard.md
+      dashboard = { preset = { header = "" } },
+    },
+  },
+  {
     "nvim-treesitter/nvim-treesitter",
-    branch = "master"
+    branch = "master",
   },
 }
