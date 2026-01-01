90fb674e00696d74da0c281d40f8d26942e93114
Stanislau Stasheuski <stanislau.stasheuski@gmail.com>
2026-01-01 22:16:57 +0200

    `nvim`: add write buffer keymaps

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,3 +1,7 @@
--- Keymaps are automatically loaded on the VeryLazy event
--- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
--- Add any additional keymaps here
+-- Default keymaps: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+
+vim.keymap.set("n", "<leader>bw", "<cmd>update<cr>", { desc = "Write Buffer" })
+vim.keymap.set("n", "<leader>bW", function()
+  vim.cmd.update()
+  Snacks.bufdelete()
+end, { desc = "Write and Delete Buffer" })
