95eb28e88ec9228e622cd789370d10ad8cd09538
Stanislau Stasheuski <stanislau.stasheuski@gmail.com>
2026-02-07 18:27:26 +0200

    `shell`: move EasyBuild to flash

--- zsh/env-lumi.sh
+++ zsh/env-lumi.sh
@@ -8,7 +8,7 @@ export SBATCH_ALLOC="${SBATCH_ACCOUNT}"
 
 export RIP_GRAVEYARD="/scratch/${SBATCH_ACCOUNT}/${USER}/.graveyard"
 
-export EBU_USER_PREFIX="/projappl/${SBATCH_ACCOUNT}/EasyBuild"
-export SPACK_USER_PREFIX="/projappl/${SBATCH_ACCOUNT}/spack"
+export EBU_USER_PREFIX="/flash/${SBATCH_ACCOUNT}/EasyBuild"
+export SPACK_USER_PREFIX="/flash/${SBATCH_ACCOUNT}/spack"
 
 export WM_PROJECT_SITE="/projappl/${SBATCH_ACCOUNT}/foamSite"
